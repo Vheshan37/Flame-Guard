5aef3665287f7680a167308cdd183f813bf7a884
Vheshan37 <vihangaheshan37@gmail.com>
2024-11-08 09:24:07 +0530

    Hibernate Entity classes
    - Department
    - District
    - SOS
    - User

---
+++ Web-Backend/FlameGuard/src/java/model/entity/Department.java
@@ -0,0 +1,79 @@
+
+package model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "district")
+public class Department implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "name", length = 45, nullable = false)
+    private String name;
+    
+    @Column(name = "code", length = 6, nullable = false)
+    private String code;
+    
+    @ManyToOne
+    @JoinColumn(name = "district_id")
+    private District district;
+    
+    @Column(name = "status", length = 6, nullable = false)
+    private String status;
+
+    public Department() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
